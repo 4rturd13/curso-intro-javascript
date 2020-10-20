be362f0977c95a1d7a618c90ebef6b88ebddb533
4rturd13 <4rturd13@gmail.com>
2020-10-20 16:20:34 -0500

    29 & 30 done

---
+++ Dia-6/29.js
@@ -0,0 +1,5 @@
+const add = (num1, nun2) => num1 + nun2
+
+console.log(add(1, 2)) // 3
+console.log(add(0, 0)) // 0
+console.log(add(245, 923)) // 1168
---
+++ Dia-6/30.js
@@ -0,0 +1,5 @@
+const hello = (str) => `Hello ${str}!`
+
+console.log(hello('Pedro')) // "Hello Pedro!"
+console.log(hello('Juan')) // "Hello Juan!"
+console.log(hello('')) // "Hello !"
